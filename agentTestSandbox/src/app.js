4bebb5bf6fa67bbbd0005129c30ed114a4e02355
YoussefKH <kh@01tek.com>
2026-02-08 03:41:56 +0100

    feat: Implement request logging with Winston

--- agentTestSandbox/src/app.js
+++ agentTestSandbox/src/app.js
@@ -1,18 +1,1 @@
-const express = require('express');
-const routes = require('./routes');
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/', routes);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Something went wrong!' });
-});
-
-module.exports = app;
+const express = require('express'); const requestLogger = require('./middleware/requestLogger'); const logger = require('./config/logger'); const app = express(); app.use(requestLogger); logger.info('Application started'); module.exports = app;
